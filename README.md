9ef03c20d0a1ee977cc7a37055312fe634da915f
Lori Boyd <lori.p.boyd@gmail.com>
2020-01-15 22:18:01 -0500

    set up file structure, created gemfile, added required files, run executable works

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Checkers
 A CLI program that runs a two player game of checkers.
+
+
+## Installing Checkers
+- Fork and clone repo
+- Run `bundle install` for dependencies
---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+require 'pry'
+
+require_relative '../lib/models/checker_board'
+require_relative '../lib/models/checker_piece'
+require_relative '../lib/models/matrix'
+require_relative '../lib/models/player'
